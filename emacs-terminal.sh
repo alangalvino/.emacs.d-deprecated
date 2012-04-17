8938df25a1536f00a39a5326ed2f6dbfa18ed967
Alan Gomes <alangalvino@gmail.com>
2012-04-17 14:58:50 -0300

    change in terminal

--- emacs-terminal.sh
+++ emacs-terminal.sh
@@ -5,6 +5,7 @@ if open -a /Applications/Emacs.app $@ 2>/dev/null
 then
     # bring emacs to front
     osascript -e 'tell application "Emacs.app" to activate'
+
 else
     # if file doesnt exist then create
     touch $@
