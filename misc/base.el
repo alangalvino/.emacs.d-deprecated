3e2603654aa0a7285efa2bef676aac1f16833a35
Alan Gomes <alangalvino@gmail.com>
2012-04-25 01:01:55 -0300

    adde base.el file

---
+++ misc/base.el
@@ -0,0 +1,8 @@
+;; Base file for local configuration, for operating system configuration go to system
+;; Don't forget to copy this file to misc/local.el
+
+;; Default folder
+(setq default-directory "~/workspace")
+
+;; Set font to Inconsolata
+(set-default-font "Inconsolata-13")
