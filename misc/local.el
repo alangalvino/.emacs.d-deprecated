84be7ee5b3f9e6e4ba2263ca2b6e5fb79c96a2ee
Alan Gomes <alangalvino@gmail.com>
2012-04-25 01:00:06 -0300

    removed local.el

--- misc/local.el
+++
@@ -1,8 +0,0 @@
-;; Base file for local configuration, for operating system configuration go to system
-;; Don't forget to add this file to .git/info/exclude for not commit local changes
-
-;; Default folder
-(setq default-directory "~/workspace")
-
-;; Set font to Inconsolata
-(set-default-font "Inconsolata-14")
