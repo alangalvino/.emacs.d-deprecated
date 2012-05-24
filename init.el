1eae8445266193d03f40f9e5f6495e8c9f807ef5
Alan Gomes <alangalvino@gmail.com>
2012-05-23 22:38:57 -0300

    Added pyflake and pep8

--- init.el
+++ init.el
@@ -20,6 +20,27 @@
 
 ;; ====== Plugins ======
 
+;; Pyflake and pep8
+(when (load "flymake" t)
+ (defun flymake-pylint-init ()
+   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                      'flymake-create-temp-inplace))
+          (local-file (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+         (list "pep8" (list "--repeat" local-file))))
+
+ (add-to-list 'flymake-allowed-file-name-masks
+              '("\\.py\\'" flymake-pylint-init)))
+
+(defun my-flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+
+(add-hook 'post-command-hook 'my-flymake-show-help)
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
 ;; Add path of the pdflatex, for my Snow Leopard
 (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
 
