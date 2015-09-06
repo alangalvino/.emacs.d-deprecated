cff32388a5ae6c49b279a17753796e968203bcf1
Alan Gomes <alan@inlocomedia.com>
2015-09-06 16:56:11 -0300

    configs for Mac OS

---
+++ config/init_mac_os.el
@@ -0,0 +1,11 @@
+;; Configurations for Mac OS
+
+;; Macs OS Command as Meta Key
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; Open just one frame
+(setq ns-pop-up-frames nil)
+
+;; Don't pass command to Mac OS, ex.: Cmd + h would not hide emacs
+(setq mac-pass-command-to-system nil)
