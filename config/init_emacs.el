be515fdbe4366f259b7b63849ce914bd8f132ec3
Alan Gomes <alan@inlocomedia.com>
2015-09-06 16:55:14 -0300

    general emacs config

---
+++ config/init_emacs.el
@@ -0,0 +1,40 @@
+;; General Emacs configs
+
+;; Loading Customize default before personal configs
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
+;; Don't make backup file
+(setq make-backup-files nil)
+
+;; Don't make autosave file
+(setq auto-save-default nil)
+
+;; Enable clipboard copy and paste
+(setq x-select-enable-clipboard t)
+
+;; Delete selected text
+(delete-selection-mode 1)
+
+;; Refresh buffers
+(global-auto-revert-mode t)
+
+;; Turn on paren match hightlighting
+(show-paren-mode 1)
+
+;; Space between code and and line number
+(setq linum-format "%d")
+
+;; Show column number
+(column-number-mode 1)
+
+;; Show lines
+(global-linum-mode 1)
+
+;; Blink cursor mode
+(setq blink-cursor-mode t)
+
+;; Set font to Inconsolata if exists
+(if (font-exists-p "Inconsolata") (set-default-font "Inconsolata-17"))
+
+;; Forcing scratch message to nil
+(setq initial-scratch-message nil)
---
+++ config/init_emacs_gui.el
@@ -0,0 +1,18 @@
+;; Emacs GUI configs
+
+;; Remove menu bar
+(menu-bar-mode -1)
+
+;; Remove toll bar
+(tool-bar-mode -1)
+
+;; Remove scroll bar
+(toggle-scroll-bar -1)
+
+;; Remove splash screen
+(setq inhibit-splash-screen t)
+
+;; Remove startup message
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(setq inhibit-startup-echo-area-message t)
