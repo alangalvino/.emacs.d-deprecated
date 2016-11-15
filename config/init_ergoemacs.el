347214b0634c81e8aae6bd6c1f79a6d16ca22427
Alan Gomes <alangalvino@gmail.com>
2016-11-15 02:47:44 -0300

    feat(ergoemacs-fix): adds line to fix ergoemacs

--- config/init_ergoemacs.el
+++ config/init_ergoemacs.el
@@ -1,6 +1,9 @@
 ;; Ergonomic keyboard layout for emacs
 (require 'ergoemacs-mode)
+
 (setq ergoemacs-theme nil)
+(setq ergoemacs-change-fixed-layout-to-variable-layout t)
+
 (setq ergoemacs-keyboard-layout "dv")
 (ergoemacs-mode 1)
 
